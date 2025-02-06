5d922a10215553d9b16e0b50aaebe188b181941f
mehithka <mehith.karacasia@gmail.com>
2025-02-06 15:41:59 +0500

    HW

--- a1.py
+++ a1.py
@@ -1,7 +1,7 @@
 test_dictionary = {'mehith' : 2 , 'Vijendra':2 , 'Karacasia':2, 'hello':1}
 print('the original dictionsary is:',test_dictionary)
 
-k=2
+k=int(input('eenter an integer'))
 res=0
 
 for key in test_dictionary:
